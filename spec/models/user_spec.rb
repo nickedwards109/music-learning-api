840737f63ccef1d96d6fab153f361854e686e41e
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-07-18 23:43:36 -0400

    Clean up user validation specs
    This commit removes a typo in the role validation spec, adds a case for creating a valid user, and adjusts indentation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,31 +7,39 @@ RSpec.describe User, type: :model do
       # single field; it tests a few fields and reasonably assumes that if some
       # validations are working and nobody is deleting validation code in the
       # model, the other validations would be working as well.
-      no_role_user = User.create(role: :admin,
-                          first_name: "FirstName1",
-                          last_name: "LastName1",
-                          email: "admin@example.com",
-                          password: "85kseOlqqp!v1@a7",
-                          password_confirmation: "85kseOlqqp!v1@a7"
-                          )
+      no_role_user = User.create(first_name: "FirstName1",
+                                 last_name: "LastName1",
+                                 email: "admin@example.com",
+                                 password: "85kseOlqqp!v1@a7",
+                                 password_confirmation: "85kseOlqqp!v1@a7"
+                                 )
       expect(no_role_user).not_to be_valid
 
       no_email_user = User.create(role: :admin,
-                           first_name: "FirstName1",
-                           last_name: "LastName1",
-                           password: "85kseOlqqp!v1@a7",
-                           password_confirmation: "85kseOlqqp!v1@a7"
-                           )
+                                  first_name: "FirstName1",
+                                  last_name: "LastName1",
+                                  password: "85kseOlqqp!v1@a7",
+                                  password_confirmation: "85kseOlqqp!v1@a7"
+                                  )
       expect(no_email_user).not_to be_valid
 
       mismatched_passwords_user = User.create(role: :admin,
-                           first_name: "FirstName1",
-                           last_name: "LastName1",
-                           email: "admin@example.com",
-                           password: "85kseOlqqp!v1@a7",
-                           password_confirmation: "nomatch"
-                           )
+                                              first_name: "FirstName1",
+                                              last_name: "LastName1",
+                                              email: "admin@example.com",
+                                              password: "85kseOlqqp!v1@a7",
+                                              password_confirmation: "nomatch"
+                                              )
       expect(mismatched_passwords_user).not_to be_valid
+
+      valid_user = User.create(role: :admin,
+                               first_name: "FirstName1",
+                               last_name: "LastName1",
+                               email: "admin@example.com",
+                               password: "85kseOlqqp!v1@a7",
+                               password_confirmation: "85kseOlqqp!v1@a7"
+                               )
+      expect(valid_user).to be_valid
     end
   end
 end
