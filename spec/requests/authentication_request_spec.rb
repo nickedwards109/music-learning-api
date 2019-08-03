240e1e81f37251ada28a265cca6b07ccab3e0946
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-08-03 18:49:43 -0400

    Specify tokens in authentication response
    The API should respond with a 200 OK and a JSON web token when a client POSTS with valid credentials. The API should respond with a 401 Unauthorized and no JSON web token when a client POSTS with invalid credentials.

--- spec/requests/authentication_request_spec.rb
+++ spec/requests/authentication_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Authentication", type: :request do
-  it "responds with a 401 Unauthorized when a client POSTS with invalid credentials" do
+  it "responds with a 401 Unauthorized and no JSON web token when a client POSTS with invalid credentials" do
     user = User.create(
                       role: :admin,
                       first_name: "FirstName1",
@@ -13,9 +13,11 @@ RSpec.describe "Authentication", type: :request do
 
     post '/api/v1/sessions?session[email]=admin%40example.com&session[password]=1234'
     expect(response).to have_http_status(401)
+    parsed_response = JSON.parse(response.body)
+    expect(parsed_response).not_to have_key("token")
   end
 
-  it "responds with a 200 OK when a client POSTS with valid credentials" do
+  it "responds with a 200 OK and a JSON web token when a client POSTS with valid credentials" do
     user = User.create(
                       role: :admin,
                       first_name: "FirstName1",
@@ -27,5 +29,8 @@ RSpec.describe "Authentication", type: :request do
 
     post '/api/v1/sessions?session[email]=admin%40example.com&session[password]=85kseOlqqp%21v1%40a7'
     expect(response).to have_http_status(200)
+    parsed_response = JSON.parse(response.body)
+    expect(parsed_response).to have_key("token")
+    expect(parsed_response["token"].split('.').count).to eq(3)
   end
 end
