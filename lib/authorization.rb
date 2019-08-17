d3aca94868e42f35691b6faf44e3af22fec2734f
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-08-17 12:48:17 -0400

    Refactor authorization logic
    This commit extracts some pieces of the authorization logic into their own distinct methods and also removes some unneeded logic.

--- lib/authorization.rb
+++ lib/authorization.rb
@@ -2,20 +2,35 @@ class Authorization
   class << self
     def authorize(request)
       if token = request.params[:token]
-        if token.split('.').count == 3
-          return false unless decoded_token = JsonWebToken.decode(token)
-          header = token.split('.')[0]
-          payload = token.split('.')[1]
-          signature = token.split('.')[2]
+        if valid_length(token)
+          header, payload, claim_signature = get_parts(token)
           key = Rails.application.credentials.secret_key_base
-          header_and_payload = header + "." + payload
-          hashed_header_and_payload = OpenSSL::HMAC.digest(OpenSSL::Digest.new("sha256"), key, header_and_payload)
-          expected_signature = Base64.urlsafe_encode64(hashed_header_and_payload).gsub("=", "")
-          return true if signature == expected_signature
+          valid_signature = sign(header, payload, key)
+          claim_signature == valid_signature
         end
       else
         return false
       end
+    end
+
+    def valid_length(token)
+      token.split('.').count == 3
+    end
+
+    def get_parts(token)
+      [ header = token.split('.')[0],
+        payload = token.split('.')[1],
+        signature = token.split('.')[2] ]
+    end
+
+    def sign(header, payload, key)
+      concatenated_header_payload = header + "." + payload
+      hashed_header_payload = hash(concatenated_header_payload, key)
+      Base64.urlsafe_encode64(hashed_header_payload).gsub("=", "")
+    end
+
+    def hash(concatenated_header_payload, key)
+      OpenSSL::HMAC.digest(OpenSSL::Digest.new("sha256"), key, concatenated_header_payload)
     end
   end
 end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,9 +4,5 @@ class JsonWebToken
       payload[:exp] = 12.hours.from_now.to_i
       JWT.encode(payload, Rails.application.credentials.secret_key_base, 'HS256')
     end
-
-    def decode(token)
-      JWT.decode(token, nil, false)
-    end
   end
 end
