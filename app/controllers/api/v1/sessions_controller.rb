6c4c04e6d377acdf621e36f4a813550464b32068
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-08-17 13:40:41 -0400

    Implement 404 response to invalid email
    This commit implements a 404 response to an authentication request containing an invalid email in order to not expose to a potential attacker whether or not the email exists in the database.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: session_params[:email])
-    if user.authenticate(session_params[:password])
-      token = JsonWebToken.encode({id: user.id})
-      render json: {token: token}, status: 200
+    if user = User.find_by(email: session_params[:email])
+      if user.authenticate(session_params[:password])
+        token = JsonWebToken.encode({id: user.id})
+        render json: {token: token}, status: 200
+      else
+        render json: {}, status: 404
+      end
     else
       render json: {}, status: 404
     end
