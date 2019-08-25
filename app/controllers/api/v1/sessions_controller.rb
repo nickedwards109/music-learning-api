b7ef51df10448049c716e4b58d65d5a8a3712c1c
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-08-25 17:35:02 -0400

    Implement the role in the token upon authentication
    When a user authenticates, the user's role is included in the payload of the JSON web token.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     if user = User.find_by(email: session_params[:email])
       if user.authenticate(session_params[:password])
-        token = JsonWebToken.encode({id: user.id})
+        token = JsonWebToken.encode({id: user.id, role: user.role})
         render json: {token: token}, status: 200
       else
         render json: {}, status: 404
