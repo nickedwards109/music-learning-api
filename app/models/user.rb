b2ea48be8b690067740704af5cb4b06d2f071178
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-08-25 16:55:37 -0400

    Add a student role to the user model
    A user can now have either of two roles: admin or student.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :role, :first_name, :last_name, :email, :password_digest, presence: true
-  enum role: [:admin]
+  enum role: [:admin, :student]
 end
