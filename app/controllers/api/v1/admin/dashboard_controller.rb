77363aaf7f976679387380c49dc7d748321c3b59
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-09-02 16:23:58 -0400

    Refactor authorization filter in dashboard controller
    This commit slightly simplifies the implementation of the authorization filter in the admin dashboard controller.

--- app/controllers/api/v1/admin/dashboard_controller.rb
+++ app/controllers/api/v1/admin/dashboard_controller.rb
@@ -2,12 +2,11 @@ class Api::V1::Admin::DashboardController < ApplicationController
   before_action :authorize
 
   def show
+    render json: {}, status: 200
   end
 
   def authorize
-    if Authorization.authorize_admin(request) == true && Authorization.verify_signature(request)
-      render json: {}, status: 200
-    else
+    if !Authorization.authorize_admin(request) || !Authorization.verify_signature(request)
      render json: {}, status: 404
    end
   end
