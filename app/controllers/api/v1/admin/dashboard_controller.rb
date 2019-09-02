db3b036567ac567c9be504c5d4299abcbb69b084
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-09-02 17:03:02 -0400

    Implement responding with the admin's name
    When an admin user sends an authorized request to '/api/v1/admin/dashboard', the server responds with JSON containing the user's first name under the key "firstName".

--- app/controllers/api/v1/admin/dashboard_controller.rb
+++ app/controllers/api/v1/admin/dashboard_controller.rb
@@ -2,12 +2,22 @@ class Api::V1::Admin::DashboardController < ApplicationController
   before_action :authorize
 
   def show
-    render json: {}, status: 200
+    token = dashboard_params[:token]
+    id = get_id(token)
+    user = User.find(id)
+    first_name = user.first_name
+    render json: {firstName: first_name}, status: 200
   end
 
   def authorize
     if !Authorization.authorize_admin(request) || !Authorization.verify_signature(request)
      render json: {}, status: 404
    end
+  end
+
+  private
+
+  def dashboard_params
+    params.permit(:token)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def get_id(token)
+    encoded_payload = token.split(".")[1]
+    decoded_payload = Base64.urlsafe_decode64(encoded_payload)
+    payload = JSON.parse(decoded_payload)
+    id = payload["id"]
+  end
 end
