73fba55e161b52ee8810707366ba2c2a39a11ba8
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-09-04 23:18:46 -0400

    Refactor encoding and decoding tokens
    This commit makes more use of the jwt-ruby library to handle the details of encoding and decoding tokens, instead of keeping an implementation from scratch of part of that functionality. This generally makes the code more readable.

--- lib/authorization.rb
+++ lib/authorization.rb
@@ -2,11 +2,8 @@ class Authorization
   class << self
     def verify_signature(request)
       if token = request.params[:token]
-        if valid_length(token)
-          header, payload, claim_signature = get_parts(token)
-          key = Rails.application.credentials.secret_key_base
-          valid_signature = sign(header, payload, key)
-          claim_signature == valid_signature
+        if JsonWebToken.decode(token)
+          return true
         end
       else
         return false
@@ -15,41 +12,10 @@ class Authorization
 
     def authorize(request, role)
       if token = request.params[:token]
-        if valid_length(token)
-          header, payload, claim_signature = get_parts(token)
-          decoded_payload = decode(payload)
-          # This gets the string indicating the role from the token.
-          # It relies on the token being structured like:
-          # "{\"id\":891,\"role:admin\"}"
-          decoded_payload.split(",")[1].split(":\"")[1].chomp("\"}") == role.to_s
-        end
+        JsonWebToken.decode(token)[0]["role"] == role.to_s
       else
         return false
       end
-    end
-
-    def valid_length(token)
-      token.split('.').count == 3
-    end
-
-    def get_parts(token)
-      [ header = token.split('.')[0],
-        payload = token.split('.')[1],
-        signature = token.split('.')[2] ]
-    end
-
-    def sign(header, payload, key)
-      concatenated_header_payload = header + "." + payload
-      hashed_header_payload = hash(concatenated_header_payload, key)
-      Base64.urlsafe_encode64(hashed_header_payload).gsub("=", "")
-    end
-
-    def hash(concatenated_header_payload, key)
-      OpenSSL::HMAC.digest(OpenSSL::Digest.new("sha256"), key, concatenated_header_payload)
-    end
-
-    def decode(encoded_string)
-      Base64.urlsafe_decode64(encoded_string)
     end
   end
 end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,5 +3,9 @@ class JsonWebToken
     def encode(payload)
       JWT.encode(payload, Rails.application.credentials.secret_key_base, 'HS256')
     end
+
+    def decode(token)
+      JWT.decode(token, Rails.application.credentials.secret_key_base, true, { algorithm: 'HS256' })
+    end
   end
 end
