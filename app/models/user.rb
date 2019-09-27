72d9a9e54a9ff2f2f573411fe109db56de7bdf46
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-09-26 22:36:04 -0400

    Enable the role of teacher
    A user can have one of three roles; admin, teacher, or student.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :role, :first_name, :last_name, :email, :password_digest, presence: true
-  enum role: [:admin, :student]
+  enum role: [:admin, :teacher, :student]
 end
