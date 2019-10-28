fd43e44d799be81d9b36cefb20a85824e91b6e13
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-10-28 11:14:20 -0400

    Specify a lesson show endpoint
    The server should respond to an authorized GET request to '/api/v1/lessons/:id' with the lesson's id, title, text, and collection of storageURLs of associated assets.

--- spec/requests/lesson_spec.rb
+++ spec/requests/lesson_spec.rb
@@ -38,4 +38,36 @@ RSpec.describe "Lessons", type: :request do
 
     expect(parsed_response).to have_key("lesson_id")
   end
+
+  it "responds to a lesson show endpoint" do
+    lesson = Lesson.create(title: "This is a title", text: "This is text.")
+    storageURL = 'http://www.example.com/assets/1'
+    asset = Asset.create(storageURL: storageURL, lesson_id: lesson.id)
+
+    user = User.create(
+                      role: :teacher,
+                      first_name: "FirstName1",
+                      last_name: "LastName1",
+                      email: "teacher@example.com",
+                      password: "85kseOlqqp!v1@a7",
+                      password_confirmation: "85kseOlqqp!v1@a7"
+                      )
+
+    # Generate a valid JSON web token that indicates a teacher role for testing purposes
+    key = Rails.application.credentials.secret_key_base
+    header = Base64.urlsafe_encode64("{\"alg\":\"HS256\"}")
+    teacher_role_payload = Base64.urlsafe_encode64("{\"id\":#{user.id},\"role\":\"teacher\"}")
+    header_and_payload = header + "." + teacher_role_payload
+    hashed_header_and_payload = OpenSSL::HMAC.digest(OpenSSL::Digest.new("sha256"), key, header_and_payload)
+    signature = Base64.urlsafe_encode64(hashed_header_and_payload).gsub("=", "")
+    teacher_token = header + "." + teacher_role_payload + "." + signature
+
+    get "/api/v1/lessons/#{lesson.id}", headers: { TOKEN: teacher_token }
+
+    response_lesson = JSON.parse(response.body)
+    expect(response_lesson["id"]).to eq(lesson.id)
+    expect(response_lesson["title"]).to eq(lesson.title)
+    expect(response_lesson["text"]).to eq(lesson.text)
+    expect(response_lesson["assets"][0]["storageURL"]).to eq(storageURL)
+  end
 end
