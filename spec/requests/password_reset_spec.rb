6d76eefe60732ef2d46b21fe09f84271e2adfec3
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-11-14 23:32:36 -0500

    Add password reset response status codes to specs

--- spec/requests/password_reset_spec.rb
+++ spec/requests/password_reset_spec.rb
@@ -29,6 +29,8 @@ RSpec.describe 'Resetting a password', type: :request do
     expect(password_reset.uuid).not_to be_nil
     expect(password_reset.user).to eq(user)
     expect(ActionMailer::Base.deliveries.last.body.raw_source).to include(password_reset.uuid)
+
+    expect(response).to have_http_status(200)
   end
 
   it 'does not start the password reset process if the provided email does not belong to a user' do
@@ -46,5 +48,7 @@ RSpec.describe 'Resetting a password', type: :request do
 
     final_emails_count = ActionMailer::Base.deliveries.count
     expect(final_emails_count).to eq(initial_emails_count)
+
+    expect(response).to have_http_status(404)
   end
 end
