8334759166b1e070a8b9153ddc639b59cd6464e0
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-12-04 18:18:05 -0500

    Add a spec for sending a student creation email
    This commit makes sure that sending user creation emails works for not only the teacher role, but also for the student role.

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -60,4 +60,30 @@ RSpec.describe "Teachers", type: :request do
     expect(afterward_emails_count).to eq(initial_emails_count + 1)
     expect(response).to have_http_status(204)
   end
+
+  it "sends a student creation email when the request includes a signed token from an admin user" do
+    admin = User.create(
+                        role: :admin,
+                        first_name: "FirstName1",
+                        last_name: "LastName1",
+                        email: "admin@example.com",
+                        password: "85kseOlqqp!v1@a7",
+                        password_confirmation: "85kseOlqqp!v1@a7"
+                        )
+
+    # Generate a valid JSON web token that indicates an admin role for testing purposes
+    key = Rails.application.credentials.secret_key_base
+    header = Base64.urlsafe_encode64("{\"alg\":\"HS256\"}")
+    admin_role_payload = Base64.urlsafe_encode64("{\"id\":#{admin.id},\"role\":\"admin\"}")
+    header_and_payload = header + "." + admin_role_payload
+    hashed_header_and_payload = OpenSSL::HMAC.digest(OpenSSL::Digest.new("sha256"), key, header_and_payload)
+    signature = Base64.urlsafe_encode64(hashed_header_and_payload).gsub("=", "")
+    admin_token = header + "." + admin_role_payload + "." + signature
+
+    initial_emails_count = ActionMailer::Base.deliveries.count
+    post '/api/v1/send_new_user_email', params: { user: { role: "student", first_name: "John", last_name: "Doe", email: "john%40example.com"} }, headers: { "TOKEN": admin_token }
+    afterward_emails_count = ActionMailer::Base.deliveries.count
+    expect(afterward_emails_count).to eq(initial_emails_count + 1)
+    expect(response).to have_http_status(204)
+  end
 end
