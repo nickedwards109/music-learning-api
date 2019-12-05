e4f8c7486d76a48cc0db84358372aba64165a7e0
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-12-04 19:09:15 -0500

    Specify a students index endpoint
    A GET to '/api/v1/students', authorized as a teacher, should result in a response with a "students" key whose value is a collection of objects showing each student's id, first name, last name, and email address.

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -86,4 +86,52 @@ RSpec.describe "Teachers", type: :request do
     expect(afterward_emails_count).to eq(initial_emails_count + 1)
     expect(response).to have_http_status(204)
   end
+
+  it "sends an index of all students" do
+    student1 = User.create(
+                           role: :student,
+                           first_name: "StudentFirstName1",
+                           last_name: "StudentLastName1",
+                           email: "student1@example.com",
+                           password: "85kseOlqqp!v1@a7",
+                           password_confirmation: "85kseOlqqp!v1@a7"
+                           )
+
+     student2 = User.create(
+                            role: :student,
+                            first_name: "StudentFirstName2",
+                            last_name: "StudentLastName2",
+                            email: "student1@example.com",
+                            password: "85kseOlqqp!v1@a7",
+                            password_confirmation: "85kseOlqqp!v1@a7"
+                            )
+
+    teacher = User.create(
+                          role: :teacher,
+                          first_name: "FirstName1",
+                          last_name: "LastName1",
+                          email: "teacher@example.com",
+                          password: "85kseOlqqp!v1@a7",
+                          password_confirmation: "85kseOlqqp!v1@a7"
+                          )
+
+    # Generate a valid JSON web token that indicates a teacher role for testing purposes
+    key = Rails.application.credentials.secret_key_base
+    header = Base64.urlsafe_encode64("{\"alg\":\"HS256\"}")
+    teacher_role_payload = Base64.urlsafe_encode64("{\"id\":#{teacher.id},\"role\":\"teacher\"}")
+    header_and_payload = header + "." + teacher_role_payload
+    hashed_header_and_payload = OpenSSL::HMAC.digest(OpenSSL::Digest.new("sha256"), key, header_and_payload)
+    signature = Base64.urlsafe_encode64(hashed_header_and_payload).gsub("=", "")
+    teacher_token = header + "." + teacher_role_payload + "." + signature
+
+    get "/api/v1/students", headers: { TOKEN: teacher_token }
+
+    parsed_response = JSON.parse(response.body)
+
+    expect(parsed_response["students"].count).to eq(2)
+    expect(parsed_response["students"][0]).to have_key("id")
+    expect(parsed_response["students"][0]).to have_key("first_name")
+    expect(parsed_response["students"][0]).to have_key("last_name")
+    expect(parsed_response["students"][0]).to have_key("email")
+  end
 end
