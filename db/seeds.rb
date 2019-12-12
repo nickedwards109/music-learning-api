ec49fb5f4582705da3bda4e5faed9e031b795ca1
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-12-11 19:09:24 -0500

    Set up database seeding for development
    To populate the database with seed data, run:
      $ rails db:seed
    
    The database seeder creates several random lessons, assets, and users. It also creates an admin with the email 'admin@example.com', a teacher with the email 'teacher@example.com', and a student with the email 'student@example.com'. Each of these users has the easy-to-remember password 'asdf'.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+include Faker
+
+# Create Users
+  # The password 'asdf' is easy to enter during development and testing.
+
+  # Create a single admin with predictable login info
+  User.create(
+    role: :admin,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: 'admin@example.com',
+    password: 'asdf',
+    password_confirmation: 'asdf'
+  )
+
+  # Create a teacher with predictable login info
+  User.create(
+    role: :teacher,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: 'teacher@example.com',
+    password: 'asdf',
+    password_confirmation: 'asdf'
+  )
+
+  # Create several random teachers
+  3.times do
+    User.create(
+      role: :teacher,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: 'asdf',
+      password_confirmation: 'asdf'
+    )
+  end
+
+  # Create a student with predictable login info
+  User.create(
+    role: :student,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: 'student@example.com',
+    password: 'asdf',
+    password_confirmation: 'asdf'
+  )
+
+  # Create several random students
+  10.times do
+    User.create(
+      role: :student,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: 'asdf',
+      password_confirmation: 'asdf'
+    )
+  end
+
+# Create Lessons
+  5.times do
+    Lesson.create({
+      title: Faker::Lorem.sentence(word_count: 3),
+      text: Faker::Lorem.paragraph(sentence_count: 10),
+      assets_attributes: [
+        {
+          storageURL:
+            "#{Faker::Internet.domain_name(domain: 'example')}/
+             #{Faker::Lorem.word}
+             .wmv"
+        }
+      ]
+    })
+  end
