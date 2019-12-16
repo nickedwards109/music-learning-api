ed11a209f2083e673f7e7741cec159b3a538a75e
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-12-15 23:32:56 -0500

    Implement authorizing assigned students to access a lesson
    A student is only authorized to access a lesson show page if a teacher has assigned the lesson to them.

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::LessonsController < ApplicationController
-  before_action :authorize_teacher
+  before_action :authorize_lesson_viewer, only: [:show]
+  before_action :authorize_teacher, only: [:index, :create]
   before_action :verify_signature
 
   def create
@@ -33,5 +34,22 @@ class Api::V1::LessonsController < ApplicationController
 
   def lesson_params
     params.require(:lesson).permit(:title, :text, assets_attributes: [ :storageURL ])
+  end
+
+  def authorize_lesson_viewer
+    # Authorize assigned students to view the lesson
+    if lesson_id = request.fullpath.split('/').last.to_i
+      token = JsonWebToken.decode(request.headers["TOKEN"])[0]
+      if token["role"] == "student"
+        student_id = token["id"]
+        if Assignment.where(lesson_id: lesson_id).where(user_id: student_id).count < 1
+          render json: {}, status: 404
+        end
+      end
+
+    # Authorize any teacher to view the lesson
+    elsif !Authorization.authorize(request, :teacher)
+     render json: {}, status: 404
+    end
   end
 end
